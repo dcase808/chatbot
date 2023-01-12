24dc6a484e3559cb78c1f85944c0e94f3cbcca29
Marcin Koczulap <xmarcinx125@gmail.com>
2023-01-12 18:41:57 +0100

    remove debug script

--- demo_blenderbot.py
+++
@@ -1,8 +0,0 @@
-from models.blenderbot.BlenderBot import BlenderBot
-
-bbot = BlenderBot('yo')
-bbot.load_model('facebook/blenderbot-1B-distill')
-id = bbot.init_conv()
-while True:
-    print('Prompt:\n')
-    print(bbot.get_response(id, input()))
