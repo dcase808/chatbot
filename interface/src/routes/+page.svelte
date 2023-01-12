51daa8be894a201f38e21f09a6c6601525103b4e
Marcin Koczulap <xmarcinx125@gmail.com>
2023-01-12 19:04:28 +0100

    change api to https

--- interface/src/routes/+page.svelte
+++ interface/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 	import { Card, FormGroup, Input, Button } from 'sveltestrap/src'
 
-	const endpoint = 'http://api.koczulap.pl'
+	const endpoint = 'https://api.koczulap.pl'
 
 	let prompt = ''
 
